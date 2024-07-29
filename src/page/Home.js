844c0d11df2490156932c16048fc8f315936e3fe
seongyurim <miruyseong@gmail.com>
2024-07-29 20:45:45 +0900

    Removed some products

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 import ProductCard from '../component/ProductCard';
 import { useSearchParams } from 'react-router-dom';
 import Footer from '../component/Footer';
@@ -8,6 +8,7 @@ const Home = () => {
 
   const [productList, setProductList] = useState([]);
   const [query, setQuery] = useSearchParams();
+  const [error, setError] = useState("");
 
   const filterProducts = (data, category) => {
     switch (category) {
@@ -35,20 +36,35 @@ const Home = () => {
     }
   }
 
-  // API 호출(검색)
+  // API 호출
   const getProducts = async() => {
-    let searchQuery = query.get('q') || "";
-    let categoryQuery = query.get('category') || "";
-    let url = `https://my-json-server.typicode.com/seongyurim/cos-shopping-router-2024/products?q=${searchQuery}&category=${categoryQuery}`;
-    let response = await fetch(url);
-    let data = await response.json();
+    try {
+      let searchQuery = query.get('q') || "";
+      let categoryQuery = query.get('category') || "";
+      let url = `https://my-json-server.typicode.com/seongyurim/cos-shopping-router-2024/products?q=${searchQuery}&category=${categoryQuery}`;
+      let response = await fetch(url);
+      let data = await response.json();
 
-    // 카테고리 필터링
-    if (categoryQuery) {
-      data = filterProducts(data, categoryQuery);
+      console.log("카테고리 필터링 전 데이터 배열: " + data);
+
+      // 카테고리 필터링
+      if (categoryQuery) {
+        data = filterProducts(data, categoryQuery);
+      }
+
+      if (data.length < 1) {
+        if (searchQuery != "") {
+          setError(`${searchQuery}와 일치하는 상품이 없습니다.`);
+        }
+        else {
+          throw new Error("결과가 없습니다.");
+        }
+      }
+      setProductList(data);
     }
-
-    setProductList(data);
+    catch (err) {
+      setError(err.message);
+    }
   }
 
   useEffect(() => {
@@ -58,16 +74,20 @@ const Home = () => {
   return (
     <div>
       <Container className="home-container">
+        {error ? (
+          <Alert variant="danger" className="error-msg">{error}</Alert>
+        ) : (
         <Row>
-          {productList.map((item) => (
-            <Col xs={12} md={6} lg={3}>
+          {productList.length > 0 &&
+           productList.map((item) => (
+            <Col xs={12} md={6} lg={4}>
               <ProductCard item={item}/>
             </Col>
           ))}
         </Row>
+        )}
       </Container>
-      <Footer />
-      
+      <Footer />      
     </div>
   );
 };
